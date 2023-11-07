c476bd2980ba3cd90e5c63646867b871d794398e
Marcus Reaiche <marcusreaiche@gmail.com>
2023-11-06 20:31:05 -0500

    Drop table operator

--- plugins/operators/drop_table.py
+++ plugins/operators/drop_table.py
@@ -4,12 +4,26 @@ from helpers.sql_queries import sql_create_tables_dict
 
 
 class DropTableOperator(BaseOperator):
+    """
+    Drop table operator
+    """
 
     query = """
     drop table if exists {table}
     """
 
     def __init__(self, redshift_conn_id, table, *args, **kwargs):
+        """
+        Drops specified table if exists
+
+        Arguments
+        ---------
+        redshift_conn_id: str (default: 'redshift')
+        Connection id to database
+
+        table: str (default: '')
+        table to be dropped
+        """
         super(DropTableOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
