d1d601b7c63d4015991e3637fe9a1f4ba6bf85b2
Marcus Reaiche <marcusreaiche@gmail.com>
2023-11-06 20:38:13 -0500

    Load dimension operator

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -62,14 +62,14 @@ def udac_example_dag():
 
     load_user_dimension_table = LoadDimensionOperator(
         task_id='Load_user_dim_table',
-        conn_id='redshift',
+        redshift_conn_id='redshift',
         table='users',
         sql=SqlQueries.user_table_insert,
         insert_mode='delete-load')
 
     load_song_dimension_table = LoadDimensionOperator(
         task_id='Load_song_dim_table',
-        conn_id='redshift',
+        redshift_conn_id='redshift',
         table='songs',
         sql=SqlQueries.song_table_insert,
         insert_mode='delete-load'
@@ -77,7 +77,7 @@ def udac_example_dag():
 
     load_artist_dimension_table = LoadDimensionOperator(
         task_id='Load_artist_dim_table',
-        conn_id='redshift',
+        redshift_conn_id='redshift',
         table='artists',
         sql=SqlQueries.artist_table_insert,
         insert_mode='delete-load'
@@ -85,7 +85,7 @@ def udac_example_dag():
 
     load_time_dimension_table = LoadDimensionOperator(
         task_id='Load_time_dim_table',
-        conn_id='redshift',
+        redshift_conn_id='redshift',
         table='time',
         sql=SqlQueries.time_table_insert,
         insert_mode='delete-load'
